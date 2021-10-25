f048520aeebf6c56384e240cd77eace5500053e1
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-10-25 11:13:19 -0300

    NLW

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,10 +1,51 @@
-import styles from './styles.module.scss';
-import logoImg from "../../assets/logo.svg"
+import styles from "./styles.module.scss";
+import logoImg from "../../assets/logo.svg";
+import { api } from "../../services/api";
+import { useEffect, useState } from "react";
 
-export function MessageList(){
-  return(
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
+};
+
+export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    api.get<Message[]>("messages/last3").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
+  return (
     <div className={styles.messageWrapper}>
       <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+
+      {messages.map(message => {
+        return(
+          <li className={styles.message} key={message.id}>
+          <p className={styles.messageContent}>
+           {message.text}
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.messageUserImage}>
+              <img
+                src={message.user.avatar_url}
+                alt={message.user.name}
+              />
+            </div>
+            <span>{message.user.name}</span>
+          </div>
+        </li>
+        )
+      })}
+      </ul>
     </div>
-  )
+  );
 }
